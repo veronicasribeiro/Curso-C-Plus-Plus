1c00e9800361893b71b79438ceaeacf8f1650427
Verônica Ribeiro <127154698+veronicasribeiro@users.noreply.github.com>
2024-01-16 23:43:48 -0300

    Add files via upload

---
+++ EstruturaBasica.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// incluse -> Comando para inclusão de bibliotecas
+// <ionstream> -> Biblioteca de comandos básicos de entrada e saída (in/out)
+
+using namespace std;
+// namespace -> Organização das bibliotecas criadas (organização de pstas)
+// std -> standard (padrão)
+
+int main(){
+	
+	cout << "Hello World!\n Welcome to the future!\n";
+	// cout -> Imprime algo na tela
+	// endl -> end line (quebra de linha)
+	
+	system("pause");
+	// Gera uma pausa e espera que eu aperte alguma tecla para continuar
+	
+	return 0;
+}
+
+// no lugar de int poderia ser void -> retorno nulo (a função não retorna nada)
+// main -> principal
+// return 0; -> Retorna 0 e terminou
+ 
